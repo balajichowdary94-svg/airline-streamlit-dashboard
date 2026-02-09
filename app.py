2679421550059ccf9b35c27d099ab9afad9e9514
balajichowdary94-svg <balajichowdary94@gmail.com>
2026-02-09 19:04:33 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("Airline Flight Delay Dashboard")
+
+@st.cache_data
+def load_data():
+    cols = [
+        'YEAR','MONTH','AIRLINE',
+        'DEPARTURE_DELAY','ARRIVAL_DELAY',
+        'CANCELLED','DISTANCE'
+    ]
+    df = pd.read_csv("flights.csv", usecols=cols)
+    df = df.sample(200000, random_state=42)
+    df = df[df["CANCELLED"] == 0]
+    df = df.dropna()
+    return df
+
+df = load_data()
